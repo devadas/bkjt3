a820be446d6a3394f86886cb46d6ea1c61fff816
Sophia Devadas <sophia.devadas@hotmail.com>
2012-06-09 10:37:47 +0100

    Initial Commit Book Source Java Threads 3rd Ed by Scott Oaks, Henry Wong

---
+++ src/main/java/javathreads/examples/ch02/example2/SwingTypeTester.java
@@ -0,0 +1,96 @@
+package javathreads.examples.ch02.example2;
+
+import javathreads.examples.ch02.CharacterDisplayCanvas;
+import javathreads.examples.ch02.CharacterEventHandler;
+import javathreads.examples.ch02.CharacterListener;
+import javathreads.examples.ch02.CharacterSource;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SwingTypeTester extends JFrame implements CharacterSource {
+
+    protected RandomCharacterGenerator producer;
+    private CharacterDisplayCanvas displayCanvas;
+    private CharacterDisplayCanvas feedbackCanvas;
+    private JButton quitButton;
+    private JButton startButton;
+    private CharacterEventHandler handler;
+    
+    public SwingTypeTester() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+        handler = new CharacterEventHandler();
+        displayCanvas = new CharacterDisplayCanvas();
+        feedbackCanvas = new CharacterDisplayCanvas(this);
+        quitButton = new JButton();
+        startButton = new JButton();
+        add(displayCanvas, BorderLayout.NORTH);
+        add(feedbackCanvas, BorderLayout.CENTER);
+        JPanel p = new JPanel();
+        startButton.setLabel("Start");
+        quitButton.setLabel("Quit");
+        p.add(startButton);
+        p.add(quitButton);
+        add(p, BorderLayout.SOUTH);
+        System.out.println("hello");
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent evt) {
+                quit();
+            }
+        });
+
+        feedbackCanvas.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent ke) {
+
+                char c = ke.getKeyChar();
+                System.out.println("Key pressed " +c);
+                if (c != KeyEvent.CHAR_UNDEFINED)
+                    newCharacter((int) c);
+            }
+        });
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                producer = new RandomCharacterGenerator();
+                displayCanvas.setCharacterSource(producer);
+                producer.start();
+                startButton.setEnabled(false);
+                feedbackCanvas.setEnabled(true);
+                feedbackCanvas.requestFocus();
+            }
+        });
+        quitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                quit();
+            }
+        });
+        pack();
+    }
+
+    private void quit() {
+        System.exit(0);
+    }
+
+    public void addCharacterListener(CharacterListener cl) {
+        handler.addCharacterListener(cl);
+    }
+
+    public void removeCharacterListener(CharacterListener cl) {
+        handler.removeCharacterListener(cl);
+    }
+
+    public void newCharacter(int c) {
+        handler.fireNewCharacter(this, c);
+    }
+
+    public void nextCharacter() {
+        throw new IllegalStateException("We don't produce on demand");
+    }
+    
+    public static void main(String args[]) {
+        new SwingTypeTester().show();
+    }
+}
